8c0d2f949c4e867c0e56792f181c850fbf5c7105
Unigwe <emmunigwe@gmail.com>
2024-03-13 18:21:48 +0100

    corrected a missing whitespace

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,5 +6,5 @@ if number > 0:
     print("{:d} is positive".format(number))
 if number < 0:
     print("{:d} is negative".format(number))
-if number==0:
+if number == 0:
     print("{:d} is zero".format(number))
