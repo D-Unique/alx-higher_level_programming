6a283194080811988596257bba2a8860b2cfb34d
Unigwe <emmunigwe@gmail.com>
2024-04-05 19:11:22 +0100

    task1

---
+++ 0x06-python-classes/1-square1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+''' This is file contains a Class - square with a size attribute'''
+
+
+class Square:
+
+    '''
+    This Class represent a Square with a size attribute
+
+    Args:
+        size (int): The size of the square.
+    '''
+
+    def __init__(self, size):
+
+        '''initialize a square object.'''
+
+        self.__size = size
