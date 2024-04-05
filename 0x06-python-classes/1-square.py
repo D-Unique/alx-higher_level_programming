7aab789ce24be6835027cf56995f329490c3b5ef
Unigwe <emmunigwe@gmail.com>
2024-04-05 19:32:39 +0100

    done with task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""This file defines the Square class,with a size attribute."""
+
+
+class Square:
+
+    '''
+    This class represents a square with a size attribute.
+
+    Args:
+        size (int): The size of the square.
+    '''
+
+    def __init__(self, size):
+
+        """Initializes a Square object."""
+
+        self.size = size
