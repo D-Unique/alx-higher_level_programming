17fe02c3cec16d3efe1e9cf6cc0866c45fb32ac7
Unigwe <emmunigwe@gmail.com>
2024-04-06 16:57:14 +0100

    done with task four

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,7 +4,7 @@
 
 
 class Square:
-
+    
     '''This is a class Square and it has a private Attribute size'''
 
     def __init__(self, size=0):
@@ -12,29 +12,29 @@ class Square:
 
         self.size = size
 
-        @property
-        def get_size(self):
-            '''This is a method that gets the value of the private attribute'''
-            return self.__size
+    @property
+    def get_size(self):
+        '''This is a method that gets the value of the private attribute'''
+        return self.__size
 
-        @value.setter
-        def size_setter(self, value):
+    @value.setter
+    def size_setter(self, value):
 
-            '''This is a method that set the private attribute size'''
+        '''This is a method that set the private attribute size'''
 
-            '''
-            Args:
-                raise:
-                    typeError: size must be an integer
-                    valueError: size must be greater than zero
-            '''
+        '''
+        Args:
+            raise:
+                typeError: size must be an integer
+                valueError: size must be greater than zero
+        '''
 
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif (size <= 0):
-                raise ValueError("size must be greater than zero")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (size <= 0):
+            raise ValueError("size must be greater than zero")
         self.__size = value
 
     def area(self):
-        '''This is a method area'''
+    '''This is a method area'''
         return self.__size * self.__size
