44528d92342a4159b4fc98a5a8d15a42d0ed9dce
Unigwe <emmunigwe@gmail.com>
2024-04-21 20:08:24 +0100

    done1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,7 +6,8 @@ and then collect arg from cmd and store in a file
 import sys
 
 if __name__ == "__main__":
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     '''import the function'''
     argument = sys.argv[1:]
