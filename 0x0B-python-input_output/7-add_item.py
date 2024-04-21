66787a302e1de7b7aeb4659fff3f0737443ddd2b
Unigwe <emmunigwe@gmail.com>
2024-04-21 19:48:19 +0100

    done

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,10 @@ if __name__ == "__main__":
     save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
     '''import the function'''
     argument = sys.argv[1:]
+    try:
+        items = load_from_json_file(add_item.json)
+    except FileNotFoundError:
+        input_0 = []
+    input_0.extend(argument)
     '''get the list of argument from the cmd except the script name'''
-    with open(add_item.json, "w") as f:
-        save_to_json_file(argument, f)
+    save_to_json_file(input_0, add_item.json)
