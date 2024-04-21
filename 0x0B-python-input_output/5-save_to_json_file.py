817e312c83fc003af6f1bef7b73e6ba264330109
Unigwe <emmunigwe@gmail.com>
2024-04-21 14:12:01 +0100

    5 and 6

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,6 +7,16 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open("filename", "w") as f:
-        json.dump(my_obj, f)
-    return f
+    '''collect object and file where Json is to be saved
+
+   args:
+        my_obj (str,dict,list) : path to python data
+        filename :
+
+    return:
+        Any : JSON str
+    '''
+
+    with open(filename, "w") as f:
+        data = json.dump(my_obj, f)
+    return data
