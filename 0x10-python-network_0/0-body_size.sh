b8232e8b9a2499f1ac76593cc3a336dc9f2889b7
Unigwe <emmunigwe@gmail.com>
2024-08-21 22:41:42 +0100

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #kes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -X GET -s "$1" | wc -c
+curl -s -w "%{size_download}" -o /dev/null "$1"
