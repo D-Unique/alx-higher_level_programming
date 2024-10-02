f004ffee803bb6e18d427408289ce224bf4d0d0a
Unigwe <emmunigwe@gmail.com>
2024-10-03 00:47:30 +0100

    feat(replace_in_list): Added a docstring to the function

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    """ This function replaces an element of
+    my_list at a specific position indicated by idx
+    """
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
     else:
