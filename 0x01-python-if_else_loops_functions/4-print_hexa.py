fe4df96923ff43854104b6720f5dea32b4bbc3f0
Unigwe <emmunigwe@gmail.com>
2024-10-13 16:17:37 +0100

    feat(4-print_hexa.py)

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
-
 """Print numbers 0 to 98 in decimal and hexadecimal."""
-for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
+
+for i in range(0, 99):
+    print("{0} = 0x{1:x}".format(i, i))
--- 0x01-python-if_else_loops_functions/test.py
+++ 0x01-python-if_else_loops_functions/test.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('a'),(ord('z') +1)):
-    if i == ord('q') or i == ord('e'):
-        continue
-    print("{0:c}".format(i),end='')
+for i in range(0, 99):
+    print("{0} = 0x{1:x}".format(i, i))
