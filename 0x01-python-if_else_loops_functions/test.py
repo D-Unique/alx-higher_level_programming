92bc05c002b33c2da6d162311e5dc9912c0f9211
Unigwe <emmunigwe@gmail.com>
2024-10-13 19:33:34 +0100

    feat(10-add.py)

--- 0x01-python-if_else_loops_functions/test.py
+++ 0x01-python-if_else_loops_functions/test.py
@@ -1,10 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-for i in range(0, 100):
-    if i == 99:
-        print(i)
-        continue
-    if i < 10:
-        print("0{}".format(i), end=',')
-    else:
-        print("{}".format(i), end=',')
+def add(a, b):
+    return a + b
